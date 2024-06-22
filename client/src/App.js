3a01a2594322c80e6b2af886d30bd5fdcfe79e62
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-23 00:15:12 +0530

    update App.js for handle created jsx files

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import UserForm from './UserForm';
+import Users from './Users';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserForm/>
+        </header>
+    </div>
+  );
+}
+
+export default App;
