d62aef008f60e08adfe3c61ef378163d30ae6064
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-23 21:29:34 +0530

    add button to navigate

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import './App.css';
-import UserForm from './UserForm';
-import Users from './Users';
 
 function App() {
+
+const navigate = useNavigate();
+
   return (
     <div className="App">
       <header className="App-header">
-        <UserForm/>
+          <h1>Welcome to User Management System</h1>
+          <button className='user-button' onClick={()=> navigate("/users")}>Users</button>
         </header>
     </div>
   );
