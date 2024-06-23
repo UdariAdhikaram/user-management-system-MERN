5402cf87d4a1d72916d0eba2342accf5dfc85731
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-23 21:30:50 +0530

    create Users.js

--- client/src/Users.js
+++ client/src/Users.js
@@ -1,5 +1,30 @@
-const Users = () =>{ // functional component(Users)
+import { Box } from "@mui/material";
+import UserForm from "./UserForm"
+import UsersTable from "./UsersTable";
 
+const users = [
+{
+    id: 1,
+    name: 'Udari'
+},
+{
+    id: 2,
+    name: 'Isuru'
+}
+];
+
+const Users = () =>{ // functional component(Users)
+    return(
+        <Box
+        sx={{
+            width: 'calc(100% - 100px)',
+            margin: 'auto',
+            marginTop: '100px'
+        }}>
+        <UserForm />
+        <UsersTable rows={users}/>
+        </Box>
+    );
 
 }
 
