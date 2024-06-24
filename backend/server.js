27d13220841952b50108586a47cfddc51dac34ad
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-25 00:51:37 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
+const port = 3001;
+const host = '127.0.0.1';
 
-const server = app.listen(3001, '127.0.0.1',() => { //'127.0.0.1' or http://localhost
-
+const server = app.listen(port, host,() => { //'127.0.0.1' or http://localhost
+    console.log(`Node server is listeninng to ${server.address().port}`)
 });
