b3de44a12218f03bb26da594086b3b52c1e9ff5d
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-25 00:47:58 +0530

    Middleware to handle URL-encoded bodies and JSON bodies

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,14 @@ app.use(cors());
 app.use(
     express.urlencoded({
         extended: true,
+
+// Middleware to handle URL-encoded bodies and JSON bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/users', (req,res) => {
+    controller.getUsers(users => {
+        res.send(users);
     })
 );
 app.use(express.json()); 
