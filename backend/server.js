558a5fc0c7e669268d50d89b864d488e4ef2d68e
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-27 23:05:43 +0530

    connect mongoose and set uri

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,26 @@
-const app = require('./app');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
 const port = 3001;
 const host = '127.0.0.1';
+const mongoose = required( 'mongoose' );
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://udariadhikaram:udari123@cluster0.jcyt83a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to mongoDB');
+    } catch (error) {
+        console.log("MongoDb erroe: ", error);
+    }
+};
+
+connect();
 
 const server = app.listen(port, host,() => { //'127.0.0.1' or http://localhost
     console.log(`Node server is listeninng to ${server.address().port}`)
