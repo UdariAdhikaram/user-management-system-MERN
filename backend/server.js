1bed20b23c28f356404dee754291a94072e5f02e
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-28 19:37:44 +0530

    update uri

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,7 @@ const mongoose = require( 'mongoose' );
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb+srv://udari:udari123@cluster0.taf5eow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
+const uri = 'mongodb+srv://adhiuda98:udari1234@cluster0.vhwtg77.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const connect = async () => {
     try {
         await mongoose.connect(uri);
@@ -26,4 +25,4 @@ const server = app.listen(port, host,() => { //'127.0.0.1' or http://localhost
     console.log(`Node server is listeninng to ${server.address().port}`)
 });
 
-app.use('./api', router);
+app.use('/api', router);
