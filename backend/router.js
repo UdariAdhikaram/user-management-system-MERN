abc5bd5c6d491c9091f0d3281fa3316c0bfaac25
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-28 19:37:32 +0530

    update post method to put and delete for update and delete users

--- backend/router.js
+++ backend/router.js
@@ -2,9 +2,9 @@ const express = require ( 'express' );
 const router = express.Router();
 const controller = require('./controller');
 
-router.get('./users', controller.getUsers);
-router.post('./createuser', controller.addUser);
-router.post('./updateuser', controller.updateUser);
-router.post('./deleteuser', controller.deleteUser);
+router.get('/users', controller.getUsers);
+router.post('/createuser', controller.addUser);
+router.put('/updateuser', controller.updateUser);
+router.delete('/deleteuser', controller.deleteUser);
 
 module.exports = router;
